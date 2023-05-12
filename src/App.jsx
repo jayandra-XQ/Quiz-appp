d1d1532a53175bf9b4a2be44a8aaba8780bb0f8f
jayandra <jayandra@jayandra.jroot>
2023-05-12 21:47:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Quiz from "./components/Quiz/Quiz";
+import { useEffect, useState } from "react";
+import { jsQuizz } from "./constants";
+
+function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const getQuestions = async () => {
+    try {
+      const response = await fetch("https://644982a3e7eb3378ca4ba471.mockapi.io/questions");
+      const questionsResponse = await response.json();
+      console.log(questionsResponse);
+      setQuestions(questionsResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return questions.length && <Quiz questions={jsQuizz.questions} />;
+}
+
+export default App;
